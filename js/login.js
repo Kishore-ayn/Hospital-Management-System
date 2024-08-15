229f23df8b39e7e0a21704cde7d913dc182258b4
Kishore <kishore.ayn@gmail.com>
2024-08-15 13:25:35 +0530

    Responsive Login Page for 375px width Screens

--- js/login.js
+++ js/login.js
@@ -14,16 +14,13 @@ document.querySelector('.js-login-page').innerHTML =  `
       <form action="login">
           <div class="login-form">
               <div class="module-input-div">
-                  <label for="module" class="module-label">Module  : </label>
                   <input type="text" placeholder="Module" name="Module" class="data-input" required>
               </div>
               <div class="username-input-div">
-                  <label for="username" class="username-label">Username : </label>
-                  <input type="text" placeholder="Username" name="Username" class="data-input" required>
+                  <input type="text" placeholder="Your Username" name="Username" class="data-input" required>
               </div>
               <div class="password-input-div">
-                  <label for="password" class="password-label">Password : </label>
-                  <input type="text" placeholder="Password" name="Password" class="data-input" required>
+                  <input type="text" placeholder="Your Password" name="Password" class="data-input" required>
               </div>
               <div class="login-button-div">
                   <button type="submit" class="login-button">Login</button> 
